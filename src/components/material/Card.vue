ecc0d40e123a73a5aa2ec9a6e1a791e93aee06e9
Rhuan Thales <rhuantst@gmail.com>
2019-10-23 20:02:08 -0300

    Alterações no front

--- src/components/material/Card.vue
+++ src/components/material/Card.vue
@@ -15,7 +15,7 @@
         :color="color"
         :class="`elevation-${elevation}`"
         class="v-card--material__header"
-        style="background-image: url('./img/camuflado.jpg');background-repeat:no-repeat; background-size:100%;"
+        style="background-image: url('./img/camuflado.jpg');background-repeat:no-repeat; background-size:100%; text-align: center;"
       >
         <slot
           v-if="!title && !text"
