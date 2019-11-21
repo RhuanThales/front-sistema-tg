bd330bea515637b4d8664f9466f81780eceb305b
Rhuan Thales <rhuantst@gmail.com>
2019-11-21 17:41:02 -0300

    Instalação do chartist.js
    Instalação do chatist.js e criação de modelo de grafico, ainda tenho que melhorar algumas coisas, estou estudando a documentação

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -43,6 +43,35 @@
                 </v-list-item-title>
               </v-card-text>
             </v-card>
+            <v-container
+              grid-list-md
+              text-xs-center
+            >
+              <v-layout
+                row
+                wrap
+              >
+                <v-flex xs12>
+                  <v-card
+                    dark
+                    color="primary"
+                  >
+                    <v-card-text class="px-0">
+                      <div class="text-xs-center">
+                        <div class="font-weight-black">Gráfico Exemplo 01</div>
+                      </div>
+                    </v-card-text>
+                  </v-card>
+                  <chartist
+                    :data="grafico1.data"
+                    :options="grafico1.options"
+                    :plugins="grafico1.plugins"
+                    :responsive-options="grafico1.responsiveOptions"
+                    type="Pie"
+                  />
+                </v-flex>
+              </v-layout>
+            </v-container>
             <!---->
           </v-flex>
         </v-layout>
@@ -53,12 +82,53 @@
 
 <script>
 import { mapState } from 'vuex'
+import Legend from 'chartist-plugin-legend'
+import Tooltip from 'chartist-plugin-tooltip'
+
 export default {
   data () {
     return {
       show: false,
       nomeUsuario: '',
-      idUsuario: ''
+      idUsuario: '',
+      grafico1: {
+        data: {
+          labels: ['A', 'B', 'C', 'D', 'E', 'F'],
+          series: ['5', '10', '15', '20', '25', '45']
+        },
+        options: {
+          donut: true,
+          donutWidth: 70,
+          chartPadding: 30,
+          showLabel: false,
+          width: '500px',
+          height: '500px',
+          plugins: [
+            Legend(),
+            Tooltip()
+          ]
+        },
+        responsiveOptions: [
+          ['screen and (max-width: 600px)', {
+            width: '200px',
+            height: '200px',
+            chartPadding: 5,
+            plugins: [
+              Legend(),
+              Tooltip()
+            ]
+          }],
+          ['screen and (min-width: 601px) and (max-width: 1400px)', {
+            width: '300px',
+            height: '300px',
+            chartPadding: 5,
+            plugins: [
+              Legend(),
+              Tooltip()
+            ]
+          }]
+        ]
+      }
     }
   },
   computed: {
@@ -86,6 +156,12 @@ export default {
         this.account.user = JSON.parse(localStorage.getItem('usuario'))
         this.nomeUsuario = this.account.user.value.nome
       }
+    },
+    grafico () {
+      try {
+        // this.grafico1.data.labels = this.unidades[0].labels
+        // this.grafico1.data.series = this.unidades[0].series
+      } catch (e) {}
     }
   }
 }
